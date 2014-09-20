3f1419f12455eafba69a071609fb30d239e77200
Tom Dalling <tom@tomdalling.com>
2014-09-20 16:34:31 +1000

    Initial commit

---
+++ rschema.rb
@@ -0,0 +1,179 @@
+require 'set'
+
+module RSchema
+  InvalidSchemaError = Class.new(StandardError)
+  ValidationFailedError = Class.new(StandardError)
+  ErrorDetails = Struct.new(:details)
+  WALKERS = []
+
+  def self.schema(&block)
+    DSL.instance_exec(&block)
+  end
+
+  def self.validate(schema, value)
+    not walk(schema, value).is_a?(RSchema::ErrorDetails)
+  end
+
+  def self.validate!(schema, value)
+    result = walk(schema, value)
+    raise(ValidationFailedError, result) if result.is_a?(RSchema::ErrorDetails)
+    result
+  end
+
+  def self.coerce(schema, value)
+    result = walk(schema, value, CoersionMapper)
+    result.is_a?(RSchema::ErrorDetails) ? nil : result
+  end
+
+  def self.coerce!(schema, value)
+    result = walk(schema, value, CoersionMapper)
+    raise(ValidationFailedError, result) if result.is_a?(RSchema::ErrorDetails)
+    result
+  end
+
+  def self.walk(schema, value, mapper = nil)
+    walker = self.walker_for(schema)
+    raise(InvalidSchemaError, schema) unless walker
+
+    value = mapper.prewalk(schema, value) if mapper
+    value = walker.walk(schema, value, mapper)
+    value = mapper.postwalk(schema, value) if mapper
+    value
+  end
+
+  def self.register_walker(klass, walker)
+    WALKERS.insert(0, [klass, walker])
+  end
+
+  def self.walker_for(schema)
+    WALKERS.each do |(klass, walker)|
+      if schema.is_a?(klass)
+        return walker
+      end
+    end
+    nil
+  end
+end
+
+module RSchema
+  OptionalHashKey = Struct.new(:key)
+
+  class DSL
+    def self._?(key)
+      OptionalHashKey.new(key)
+    end
+  end
+end
+
+module RSchema
+  module CoersionMapper
+    def self.prewalk(schema, value)
+      if schema == Integer && value.is_a?(String)
+        value.to_i #TODO: this conversion needs to be more robust
+      elsif schema == Float && value.is_a?(String)
+        value.to_f #TODO: this conversion needs to be more robust
+      elsif schema == Symbol && value.is_a?(String)
+        value.to_sym #TODO: this conversion needs to be more robust
+      elsif schema == String && value.is_a?(Symbol)
+        value.to_s
+      elsif schema == Array && value.is_a?(Set)
+        value.to_a
+      elsif schema == Set && value.is_a?(Array)
+        Set.new(value)
+      else
+        value
+      end
+    end
+
+    def self.postwalk(schema, value)
+      value
+    end
+  end
+end
+
+module RSchema
+  module ClassWalker
+    def self.walk(schema, value, mapper)
+      if value.is_a?(schema)
+        value
+      else
+        RSchema::ErrorDetails.new("is not a #{schema}")
+      end
+    end
+  end
+end
+
+module RSchema
+  module ArrayWalker
+    def self.walk(schema, value, mapper)
+      fixed_size = (schema.size != 1)
+
+      if not value.is_a?(Array)
+        RSchema::ErrorDetails.new("is not an Array")
+      elsif fixed_size && value.size != schema.size
+        RSchema::ErrorDetails.new("does not have #{schema.size} elements")
+      else
+        value.each.with_index.map do |subvalue, idx|
+          subschema = (fixed_size ? schema[idx] : schema.first )
+          subvalue_walked = RSchema.walk(subschema, subvalue, mapper)
+          if subvalue_walked.is_a?(RSchema::ErrorDetails)
+            break RSchema::ErrorDetails({ idx => subvalue_walked.details })
+          end
+          subvalue_walked
+        end
+      end
+    end
+  end
+end
+
+module RSchema
+  module HashWalker
+    InvalidSchemaHashKeyError = Class.new(StandardError)
+
+    def self.walk(schema, value, mapper)
+      # must be a hash
+      if not value.is_a?(Hash)
+        return RSchema::ErrorDetails.new("is not a Hash")
+      end
+
+      # extract details from the schema
+      required_keys = Set.new
+      all_subschemas = {}
+      schema.each do |(k, subschema)|
+        if k.is_a?(OptionalHashKey)
+          all_subschemas[k.key] = subschema
+        else
+          required_keys << k
+          all_subschemas[k] = subschema
+        end
+      end
+
+      # check for extra keys that shouldn't be there
+      extraneous = value.keys.reject{ |k| all_subschemas.has_key?(k) }
+      if extraneous.size > 0
+        return RSchema::ErrorDetails.new({"has extraneous keys" => extraneous})
+      end
+
+      # check for required keys that are missing
+      missing_requireds = required_keys.reject{ |k| value.has_key?(k) }
+      if missing_requireds.size > 0
+        return RSchema::ErrorDetails.new({"is missing required keys" => missing_requireds})
+      end
+
+      # walk the subvalues
+      value.reduce({}) do |accum, (k, subvalue)|
+        subvalue_walked = RSchema.walk(all_subschemas[k], subvalue, mapper)
+        if subvalue_walked.is_a?(RSchema::ErrorDetails)
+          break RSchema::ErrorDetails.new({ k => subvalue_walked.details })
+        end
+        accum[k] = subvalue_walked
+        accum
+      end
+    end
+  end
+end
+
+RSchema.register_walker(Class, RSchema::ClassWalker)
+RSchema.register_walker(Array, RSchema::ArrayWalker)
+RSchema.register_walker(Hash, RSchema::HashWalker)
+
---
+++ rschema_spec.rb
@@ -0,0 +1,108 @@
+require_relative 'rschema'
+
+RSpec.describe RSchema do
+  describe '#validate' do
+    it 'validates scalars' do
+      schema = String
+      expect(RSchema.validate(schema, 'Johnny')).to be(true)
+      expect(RSchema.validate(schema, 5)).to be(false)
+    end
+
+    it 'validates arrays' do
+      schema = [String]
+      expect(RSchema.validate(schema, ['cat', 'bat'])).to be(true)
+      expect(RSchema.validate(schema, [6, 'bat'])).to be(false)
+    end
+
+    it 'validates fixed-length arrays' do
+      schema = [String, Integer]
+
+      expect(RSchema.validate(schema, ['cat', 5])).to be(true)
+
+      expect(RSchema.validate(schema, ['cat'])).to be(false)
+      expect(RSchema.validate(schema, ['cat', 5, 'horse'])).to be(false)
+    end
+
+    it 'validates hashes (required keys)' do
+      schema = {
+        name: String,
+        age: Integer,
+      }
+
+      expect(RSchema.validate(schema, name: 'Jimmy', age: 25)).to be(true)
+
+      expect(RSchema.validate(schema, name: 'Jimmy', age: 25, extra: true)).to be(false)
+      expect(RSchema.validate(schema, name: 5, age: 25)).to be(false)
+      expect(RSchema.validate(schema, age: 25)).to be(false)
+    end
+
+    it 'validates optional hash keys' do
+      schema = RSchema.schema {{
+        required: String,
+        _?(:optional) => String,
+      }}
+
+      expect(RSchema.validate(schema, required: 'hello')).to be(true)
+      expect(RSchema.validate(schema, required: 'hello', optional: 'world')).to be(true)
+
+      expect(RSchema.validate(schema, optional: 'world')).to be(false)
+    end
+
+    it 'handles arbitrary nesting' do
+      schema = {
+        list_name: String,
+        list_items: [{
+          item_id: Integer,
+          item_name: String,
+        }],
+      }
+
+      good_value = {
+        list_name: 'Blog Posts',
+        list_items: [
+          {item_id: 1, item_name: 'Hello world'},
+          {item_id: 3, item_name: 'Hello moon'},
+          {item_id: 9, item_name: 'Hello sun'},
+        ]
+      }
+
+      bad_value = {
+        list_name: 'Blog Posts',
+        list_items: [
+          [1, 'Hello world'],
+          [3, 'Hello moon'],
+          [9, 'Hello sun'],
+        ]
+      }
+
+      expect(RSchema.validate(schema, good_value)).to be(true)
+      expect(RSchema.validate(schema, bad_value)).to be(false)
+    end
+  end
+
+  describe '#coerce' do
+    it 'coerces String => Integer' do
+      expect(RSchema.coerce(Integer, '5')).to eq(5)
+    end
+
+    it 'coerces String => Float' do
+      expect(RSchema.coerce(Float, '1.23')).to eq(1.23)
+    end
+
+    it 'coerces String => Symbol' do
+      expect(RSchema.coerce(Symbol, 'hello')).to eq(:hello)
+    end
+
+    it 'coerces Symbol => String' do
+      expect(RSchema.coerce(String, :hello)).to eq('hello')
+    end
+
+    it 'coerces Array => Set' do
+      expect(RSchema.coerce(Set, [1, 2, 2, 3])).to eq(Set.new([1, 2, 3]))
+    end
+
+    it 'coerces Set => Array' do
+      expect(RSchema.coerce(Array, Set.new([1, 2, 3]))).to contain_exactly(1, 2, 3)
+    end
+  end
+end
