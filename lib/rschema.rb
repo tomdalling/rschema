1f710d657ac5caee5072a51e09184f5906ba8d81
Gert Goet <gert@thinkcreate.nl>
2015-10-31 14:38:11 +0100

    Give pointer what would be valid

--- lib/rschema.rb
+++ lib/rschema.rb
@@ -253,7 +253,7 @@ module RSchema
       if value_set.include?(value_walked)
         value_walked
       else
-        RSchema::ErrorDetails.new(value_walked, "is not a valid enum member")
+        RSchema::ErrorDetails.new(value_walked, "is not one of #{value_set.to_a}")
       end
     end
 
